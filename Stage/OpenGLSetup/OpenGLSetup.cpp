01c8526c1af602932742400a5c63c8a7aaf8c004
V Cavallaro <145499576+ChenzCat@users.noreply.github.com>
2025-02-20 17:13:17 -0500

    Added C++ File

--- Stage/OpenGLSetup/OpenGLSetup.cpp
+++ Stage/OpenGLSetup/OpenGLSetup.cpp
@@ -1,4 +1,4 @@
-﻿#include <GL/glut.h>
+#include <GL/glut.h>
 #include <IrrKlang/irrKlang.h>
 using namespace irrklang;
 
@@ -7,12 +7,47 @@ using namespace irrklang;
 #define WIN_H 600 // in pixels
 #define WIN_W 600
 
+// Creates sound engine
+ISoundEngine* SoundEngine = createIrrKlangDevice();
+
 void init(void) {
 
 	glClearColor(0.0, 0.0, 0.0, 1.0); // clear the window screen
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+}
+
+void procKeys(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'p': // Pause all sound
+		SoundEngine->setAllSoundsPaused(true);
+		break;
+	case 'm': // Unpause all sound
+		SoundEngine->setAllSoundsPaused(false);
+		break;
+	case 'd': // Drop our sound engine
+		SoundEngine->drop();
+		break;
+	case 'a': // Add a sound engine
+		SoundEngine = createIrrKlangDevice();
+		break;
+	case 27: // escape
+		exit(0);
+	}
+}
+
+void mouse(int btn, int state, int x, int y)
+{
+	// Add music
+	if (btn == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+		SoundEngine->play2D("audio/breakout.mp3", true);
+
+	// Stops all sounds
+	if (btn == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+		SoundEngine->stopAllSounds();
 }
 
 void MyDisplay() {
@@ -27,11 +62,16 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_RGB); // RGB mode
 	glutInitWindowSize(WIN_W, WIN_H); // window size
 	glutInitWindowPosition(WIN_X, WIN_Y);
-	glutCreateWindow("(Name here) (KSUID here)");
+	glutCreateWindow("Sound/Music Example");
 
 	init();
 
 	glutDisplayFunc(MyDisplay); // call the drawing function
+
+	// Gets mouse inputs
+	glutMouseFunc(mouse);
+
+	glutKeyboardFunc(procKeys);
 
 	glutMainLoop();
 	return 0;
