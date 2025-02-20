40f177f2b0845209e936976c03d92443c069f3e1
V Cavallaro <145499576+ChenzCat@users.noreply.github.com>
2025-02-20 15:16:02 -0500

    added Setup

---
+++ Stage/OpenGLSetup/OpenGLSetup.cpp
@@ -0,0 +1,38 @@
+﻿#include <GL/glut.h>
+#include <IrrKlang/irrKlang.h>
+using namespace irrklang;
+
+#define WIN_X 100
+#define WIN_Y 100
+#define WIN_H 600 // in pixels
+#define WIN_W 600
+
+void init(void) {
+
+	glClearColor(0.0, 0.0, 0.0, 1.0); // clear the window screen
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+}
+
+void MyDisplay() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Code here
+
+	glFlush();
+}
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB); // RGB mode
+	glutInitWindowSize(WIN_W, WIN_H); // window size
+	glutInitWindowPosition(WIN_X, WIN_Y);
+	glutCreateWindow("(Name here) (KSUID here)");
+
+	init();
+
+	glutDisplayFunc(MyDisplay); // call the drawing function
+
+	glutMainLoop();
+	return 0;
+}
